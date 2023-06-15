4adadce9d081675222bb9e3f25eb0c704303a082
n.polyakova <natli.d26.l8@gmail.com>
2023-06-15 19:50:53 +0300

    Initial commit

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UnitTest {
+
+    @Test
+    public void shouldBeSuccessful() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
